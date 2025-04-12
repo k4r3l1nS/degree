5fd42f56c5a76fc4ad1017376f10cb1ead53243a
Kurchenkov.Maksim <maksim.kurchenkov@unicardtech.ru>
2025-04-12 11:58:59 +0300

    [feature] Обновление валют при запуске приложения

--- src/main/java/com/practice/demo/PracticeApplication.java
+++ src/main/java/com/practice/demo/PracticeApplication.java
@@ -1,13 +1,27 @@
 package com.practice.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.practice.demo.service.ScheduledService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class PracticeApplication {
 
-	public static void main(String[] args) { SpringApplication.run(PracticeApplication.class, args);}
+	private static final Logger log = LoggerFactory.getLogger(PracticeApplication.class);
+
+	public static void main(String[] args) {
+		try {
+			ConfigurableApplicationContext context = SpringApplication.run(PracticeApplication.class, args);
+			context.getBean("scheduledService", ScheduledService.class).renewCurrencyRates();
+		} catch (JsonProcessingException e) {
+			log.warn("Не удалось обновить курс валют при запуске приложения: {}", e.getMessage());
+		}
+	}
 
 }
